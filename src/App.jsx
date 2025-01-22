1aef4b3bddbc973548fdbd65cd11ca6a2badcb34
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-01-21 21:06:13 -0300

    Adicionando o prettier no projeto e a base de como funcionará os estágios do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+// CSS
 import "./app.css";
+
+//React
+import { useCallback, useEffect, useState } from "react";
+
+// Data
+import { wordsList } from "./data/words";
+
+// Components
 import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+    { id: 1, name: "start" },
+    { id: 2, name: "game" },
+    { id: 3, name: "end" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <StartScreen />
-    </div>
-  );
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList);
+
+    return (
+        <div className="App">
+            {gameStage === "start" && <StartScreen />}
+            {gameStage === "game" && <Game />}
+            {gameStage === "end" && <GameOver />}
+        </div>
+    );
 }
 
 export default App;
